1feb6ba4a461ef37f843e3cab7f281744bcbcb9a
Hogu59 <dbstjdguswkd@naver.com>
2023-11-10 00:04:11 +0900

    FEAT: Implement getting input "expected visit date"
    1. Print two necessary lines.
    2. Implement getting input method. get Input again if the input was wrong.
    3. Implement validating input method. (null, out of bound, not a number)

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -1,5 +1,34 @@
 package christmas
 
+import camp.nextstep.edu.missionutils.*
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
+
 fun main() {
-    TODO("프로그램 구현")
+    println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n")
+    println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
+    val visitDate = getVisitDate()
+
+
+}
+
+fun getVisitDate() : Int {
+    val visitDate = Console.readLine()
+    return try {
+        validateVisitDate(visitDate)
+    } catch (exception : IllegalArgumentException) {
+        println(exception.message)
+        getVisitDate()
+    }
+}
+
+fun validateVisitDate(visitDate : String) : Int {
+    try {
+        require(visitDate.isNotEmpty()) { "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요." }
+        val validVisitDate = visitDate.toInt()
+        require(validVisitDate in 1..< 32) { "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요." }
+    } catch (exception : NumberFormatException) {
+        throw IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.")
+    }
+    return visitDate.toInt()
 }
