1f52518aad890b6385e4fab09c5c9bc97c97324a
Hogu59 <dbstjdguswkd@naver.com>
2023-11-12 09:57:33 +0900

    FEAT: Implement Output part
    1. Print orderList.
    2. Print price before discount.
    3. Print complimentary list.
    4. Print benefit list.
    5. Print total benefit price.
    6. Print expected payment price.
    7. Print event Badge.

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -3,6 +3,7 @@ package christmas
 import camp.nextstep.edu.missionutils.*
 import java.lang.IllegalArgumentException
 import java.lang.NumberFormatException
+import java.text.DecimalFormat
 
 val menuList = listOf<String>(
     "양송이수프",
@@ -18,6 +19,33 @@ val menuList = listOf<String>(
     "레드와인",
     "샴페인"
 )
+val appetizerMenu = listOf(
+    "양송이수프",
+    "타파스",
+    "시저샐러드"
+)
+val mainMenu = listOf(
+    "티본스테이크",
+    "바비큐립",
+    "해산물파스타",
+    "크리스마스파스타"
+)
+val dessertMenu = listOf(
+    "초코케이크",
+    "아이스크림"
+)
+val drinkMenu = listOf(
+    "제로콜라",
+    "레드와인",
+    "샴페인"
+)
+
+val priceList = listOf(6000, 5500, 8000, 55000, 54000, 35000, 25000, 15000, 5000, 3000, 60000, 25000)
+
+val menuPriceMap = HashMap<String, Int>()
+
+data class Order(val name: String, val num: Int)
+private val decimalFormat = DecimalFormat("#,###")
 
 fun main() {
     println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.")
@@ -26,7 +54,33 @@ fun main() {
 
     println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)")
     val orders = getOrders()
+    println("12월 ${visitDate}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n")
+    println("<주문 메뉴>")
+    for (i in orders.indices) {
+        println("${orders[i].name} ${orders[i].num}개")
+    }
 
+    //Todo
+    //Decimal Format
+
+    println("\n<할인 전 총주문 금액>")
+    println("${decimalFormat.format(getTotalPrice(orders))}원")
+
+    println("\n<증정 메뉴>")
+    println("${checkComplimentary(getTotalPrice(orders))}")
+
+    println("\n<혜택 내역>")
+    printDiscountList(visitDate, orders)
+
+    println("\n<총혜택 금액>")
+    if(getTotalDiscountAmount(visitDate, orders) == 0) println("없음")
+    else println("-${decimalFormat.format(getTotalDiscountAmount(visitDate, orders))}원")
+
+    println("\n<할인 후 예상 결제 금액>")
+    println("${decimalFormat.format(getTotalPrice(orders)- getTotalDiscountAmount(visitDate, orders) + getComplimentaryDiscountAmount(orders))}원")
+
+    println("\n<12월 이벤트 배지>")
+    println("${getBadge(visitDate, orders)}")
 }
 
 fun getVisitDate(): Int {
@@ -68,6 +122,8 @@ fun validateOrders(orders: String): List<Order> {
         //contains space
         require(!orders.contains(" ")) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
         val orderList = orders.split(',')
+        //음료만 주문한 경우도
+        //TODO()
         require(checkNameOfMenu(orderList)) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
         require(checkDuplicateMenu(orderList)) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
         require(checkNumberOfMenu(orderList)) { "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요." }
@@ -92,7 +148,7 @@ fun checkDuplicateMenu(orderList: List<String>): Boolean {
     val list = mutableListOf<String>()
     for (i in orderList.indices)
         list.add(orderList[i].split('-')[0])
-    if(list.distinct().size == orderList.size) return true
+    if (list.distinct().size == orderList.size) return true
     return false
 }
 
@@ -106,8 +162,109 @@ fun checkTotalNumberOfMenu(orderList: List<String>): Boolean {
 
 fun checkNumberOfMenu(orderList: List<String>): Boolean {
     for (i in orderList.indices)
-        if(orderList[i].split('-')[1].toInt() !in 1 .. 20) return false
+        if (orderList[i].split('-')[1].toInt() !in 1..20) return false
     return true
 }
 
-data class Order(val menu: String, val num: Int)
+
+fun menuPriceMapInit() {
+    for (i in menuList.indices)
+        menuPriceMap[menuList[i]] = priceList[i]
+}
+
+fun getTotalPrice(orders: List<Order>): Int {
+    menuPriceMapInit()
+    var sum = 0
+    orders.forEach { sum += menuPriceMap.getValue(it.name) * it.num }
+    return sum
+}
+
+fun checkComplimentary(price: Int): String {
+    return if (price >= 120000) "샴페인 1개"
+    else "없음"
+}
+
+fun printDiscountList(date: Int, orders: List<Order>) {
+    var discountList = ""
+    if(getDdayDiscountAmount(date) != 0){
+        //print("크리스마스 디데이 할인: -")
+        //println("${getDdayDiscountAmount(date)}원")
+        discountList += "크리스마스 디데이 할인: -"
+        discountList += "${getDdayDiscountAmount(date)}원\n"
+    }
+    if(getWeeklyDiscountAmount(date, orders) != 0) {
+        if(date % 7 == 1 || date %7 == 2)
+            //println("평일 할인: -${getWeeklyDiscountAmount(date,orders)}원")
+            discountList += "평일 할인: -${getWeeklyDiscountAmount(date,orders)}원\n"
+        //else println("주말 할인: -${getWeeklyDiscountAmount(date, orders)}원")
+        else discountList += "주말 할인: -${getWeeklyDiscountAmount(date, orders)}원\n"
+    }
+
+    if(getSpecialDiscountAmount(date) != 0){
+        //print("특별 할인: -")
+        //println("${getSpecialDiscountAmount(date)}원")
+        discountList += "특별 할인: -${getSpecialDiscountAmount(date)}원\n"
+    }
+    if(checkComplimentary(getTotalPrice(orders)) != "없음")
+        //println("증정 이벤트: -25,000원")
+        discountList += "증정 이벤트: -25,000원\n"
+
+    if(discountList.isEmpty()) println("없음")
+    print(discountList)
+}
+
+fun getTotalDiscountAmount(date: Int, orders: List<Order>): Int {
+    var totalDiscountAmount = 0
+    totalDiscountAmount += getDdayDiscountAmount(date)
+    totalDiscountAmount += getWeeklyDiscountAmount(date, orders)
+    totalDiscountAmount += getSpecialDiscountAmount(date)
+    totalDiscountAmount += getComplimentaryDiscountAmount(orders)
+    return totalDiscountAmount
+}
+
+fun getDdayDiscountAmount(date: Int): Int {
+    if (date > 25) return 0
+    return 1000 + (date - 1) * 100
+}
+
+fun getSpecialDiscountAmount(date: Int): Int {
+    if (date % 7 == 3 || date == 25) return 1000
+    return 0
+}
+
+fun getWeeklyDiscountAmount(date: Int, orders : List<Order>) : Int {
+    if(date % 7 == 1 || date % 7 == 2) return getWeekendDiscountAmount(date, orders)
+    return getWeekdayDiscountAmount(date, orders)
+}
+
+fun getWeekendDiscountAmount(date: Int, orders: List<Order>): Int {
+    var discountAmount = 0
+    orders.forEach {
+        if(mainMenu.contains(it.name))
+            discountAmount += it.num * 2023
+    }
+    return discountAmount
+}
+
+fun getWeekdayDiscountAmount(date: Int, orders: List<Order>): Int {
+    var discountAmount = 0
+    orders.forEach {
+        if(dessertMenu.contains(it.name))
+            discountAmount += it.num * 2023
+    }
+    return discountAmount
+}
+
+fun getComplimentaryDiscountAmount(orders: List<Order>) : Int {
+    if(checkComplimentary(getTotalPrice(orders)) != "없음") return 25000
+    return 0
+}
+
+fun getBadge(date: Int, orders: List<Order>) : String {
+    return when(getTotalDiscountAmount(date, orders)) {
+        in 0 ..< 5000 -> "없음"
+        in 5000 ..< 10000 -> "별"
+        in 10000 ..< 20000 -> "트리"
+        else -> "산타"
+    }
+}
